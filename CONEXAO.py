899412ab211de2c5bd9553a8ab3c282b926ff7f2
Hyaguiin <137925587+Hyaguiin@users.noreply.github.com>
2023-06-27 15:40:42 -0300

    Conexao.py
    adicionar sexo ao sql

---
+++ CONEXAO.py
@@ -0,0 +1,65 @@
+import mysql.connector
+
+class Conexao:
+        def __init__(self):
+            self.connection = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='',
+                database='ouvidoria',
+            )
+            self.cursor = self.connection.cursor()
+
+        def listar_usuarios(self):
+            sql='SELECT * FROM usuarios'
+
+            self.cursor.execute(sql)
+            lista_usuario= self.cursor.fetchall()
+
+            return lista_usuario
+
+        def cadastrar_usuario(self, nome, usuario, senha):
+                sql='INSERT INTO usuarios(nome, usuario, senha) VALUES (%s,%s,%s)'
+                data=(nome, usuario, senha)
+
+                self.cursor.execute(sql, data)
+                self.connection.commit()
+
+                userId = self.cursor.lastrowid
+                return userId
+
+        def deletar_usuario(self, id):
+            sql='DELETE from usuarios where id=%s'
+            data=(id,)
+
+            self.cursor.execute(sql,data)
+            self.connection.commit()
+
+            recordAffect = self.cursor.rowcount
+
+            return recordAffect
+
+        def atualizar_usuario(self, nome, usuario, id):
+            sql = 'UPDATE usuarios set nome=%s, usuario=%s where id=%s'
+            data = (nome, usuario, id)
+
+            self.cursor.execute(sql, data)
+            self.connection.commit()
+
+            lista_afetada = self.cursor.rowcount
+
+            return lista_afetada
+
+        def close_conexao(self):
+            self.cursor.close()
+            self.connection.close()
+
+
+conexao = Conexao()
+
+lista_usuarios = conexao.listar_usuarios()
+for usuario in lista_usuarios:
+        print(usuario)
+        print(usuario[0], usuario[1])
+
+conexao.close_conexao()
